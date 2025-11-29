aeca49b0b19ecd046aeaf1dc2f315841cb9df1c6
Ivan Tkachuk <ivtkac@gmail.com>
2025-11-29 19:15:05 +0200

    Quartz sync: Nov 29, 2025, 7:15 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Notes about everything (maybe)",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
